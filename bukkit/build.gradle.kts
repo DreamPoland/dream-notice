6a1403493bce0a8693d2037eadbb899acf523d05
Ravis96 <ravis96@spoko.pl>
2024-10-08 15:15:29 +0200

    Bump dream-utilities to 1.5.1

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -10,8 +10,8 @@ dependencies {
     compileOnly("org.spigotmc:spigot-api:1.8.8-R0.1-SNAPSHOT")
 
     // -- dream-utilities --
-    implementation("cc.dreamcode:utilities:1.5.0")
-    implementation("cc.dreamcode:utilities-bukkit:1.5.0")
+    implementation("cc.dreamcode:utilities:1.5.1")
+    implementation("cc.dreamcode:utilities-bukkit:1.5.1")
 
     // -- x-series --
     implementation("com.github.cryptomorin:XSeries:11.3.0")
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     // -- dream-utilities --
-    implementation("cc.dreamcode:utilities:1.5.0")
+    implementation("cc.dreamcode:utilities:1.5.1")
 
     // -- okaeri-placeholders --
     implementation("eu.okaeri:okaeri-placeholders-core:5.0.1")
